e8d0cca2eb8256291f330232dbb9502a8f5c2ae4
“Yung“ <kakoiievo@gmail.com>
2019-01-14 14:54:21 +0800

    updateWaterTest

--- BabyPapaGo/WeatherDataModel.swift
+++ BabyPapaGo/WeatherDataModel.swift
@@ -17,25 +17,25 @@ class WeatherDataModel {
     
     
     //更新狀態圖標
-    func updataIcon(conditionId:Int) -> String
-    {
-        switch conditionId {
-        case 0...232:
-            return "雷雨"
-        case 300...531:
-            return "下雨"
-        case 600...622:
-            return "下雪"
-        case 701...781:
-            return "起霧"
-        case 800:
-            return "太陽"
-        case 801...804:
-            return "多雲"
-        default:
-            return "太陽"
-        }
-    }
+//    func updataIcon(conditionId:Int) -> String
+//    {
+//        switch conditionId {
+//        case 0...232:
+//            return "雷雨"
+//        case 300...531:
+//            return "下雨"
+//        case 600...622:
+//            return "下雪"
+//        case 701...781:
+//            return "起霧"
+//        case 800:
+//            return "太陽"
+//        case 801...804:
+//            return "多雲"
+//        default:
+//            return "太陽"
+//        }
+//    }
     
 }
 
